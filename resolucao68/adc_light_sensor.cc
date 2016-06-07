6c717c8c699bde10e64e31a6fc196b4a62f70d16
Lucas Finger Roman <lfrfinger@gmail.com>
2016-06-07 19:30:27 -0300

    Testando usar a bateria para o sensor de luz.

--- resolucao68/adc_light_sensor.cc
+++ resolucao68/adc_light_sensor.cc
@@ -28,7 +28,31 @@
 #include <headers/array_operations.h>
 #include <classes/MyClass.h>
 
+const int lightBase = 3760;  // = power[i] = 0;
 
+const int lightMax = 3763; // = powerí] = 100;
+
+/*
+ * 
+ * 3761  -> power[i] = 33; 
+ * 
+ * Power of light = power[i] = 90;
+ * 
+ * 90 - 33;
+ * 
+ * 57
+ * 
+ * (100 - adc.get() em %) -> % do power a aplicar.
+ * 
+ * 100 - 33 = 66
+ * 
+ * 66%
+ * 
+ * 60
+ * 
+ */
+
+Battery batt;
 
 /**
  * #define __SYS_NS	   System
@@ -36,7 +60,25 @@
  */
 __USING_SYS;
 
+//Args -> power[i]
+typedef int ( * LightVariationFunc ) ( int );
 
+
+int DefaultLightVal( int val ) {
+  return val;
+}
+
+int Alternative1 ( int val ) {
+  int parcial = val - ( int ) 100 *  ( ( double ) batt.get() ) / lightMax;
+  if ( parcial < 0 ) 
+    return 0;
+  return parcial;
+}
+
+int Alternative2 ( int val ) {
+  int perc = ( int ) 100 * ( double ) ( batt.get() / lightMax );
+  return ( int ) perc * ( ( double ) val ) / 100 ;
+}
 
 /**
  * Main function entry point. It need the light sensor attached to the EPOSMotes2 board to work.
@@ -51,8 +93,8 @@ int main()
     PRINTLN( 1, "EposMotesII app ADC_LIGHT_SENSOR initing..." ); 
     PRINTLN( 1, "Program version: " << PROGRAM_VERSION );
     
-    ADC adc( ADC::SINGLE_ENDED_ADC7 );
-    adc.enable();
+//     adc = ADC( ADC::SINGLE_ENDED_ADC7 );
+//     adc.enable();
     
     int i = 0;
     
@@ -62,8 +104,10 @@ int main()
         // PRINTLN( 1, "THE GREAT LIGHT VALUE IS: " << adc.sample() );
         if ( i == 100 )
             cout << "Hoi!! \n";
-        
-        cout << "THE GREAT LIGHT VALUE IS: " << adc.sample() << endl;
+        cout << "Iteration" << endl;
+	cout << "THE GREAT LIGHT VALUE IS " << batt.get() << endl;
+        //cout << Alternative1(50) << endl;
+	//cout << Alternative2(50) << endl;
         // PRINTLN( 1, "THE WRONG GUY: " << adc.get() );
     }
     
